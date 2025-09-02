6ed5ff05210dc33b8a5652aebf6a94207b7c6911
Jungeun Kim <jungeunkim@Jungeuns-MacBook-Air.local>
2025-09-03 00:01:05 +1000

    fix: simplify vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: path.resolve(__dirname, './public'),
+  root: 'public',
   build: {
-    outDir: path.resolve(__dirname, './dist'),
-    emptyOutDir: true
+    outDir: '../dist'
   }
 })
