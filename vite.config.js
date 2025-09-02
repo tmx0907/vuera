c971f72e5a12b569dd6054857e3e038f4a0fc6c8
Jungeun Kim <jungeunkim@Jungeuns-MacBook-Air.local>
2025-09-02 23:55:20 +1000

    fix: use absolute paths for vite build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: 'public',
+  root: path.resolve(__dirname, './public'),
   build: {
-    outDir: '../dist',
+    outDir: path.resolve(__dirname, './dist'),
     emptyOutDir: true
   }
 })
