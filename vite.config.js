89c348f5c00352f24aaa94c82f08afc0b284b09a
tmx0907 <tmx0907@gmail.com>
2025-09-07 00:31:30 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-+ import { defineConfig } from "vite";
-+ import react from "@vitejs/plugin-react";
-...
-+ publicDir: "public",
-+ build: { outDir: "dist" }
-+ });
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: { outDir: "dist" }
+});
 
