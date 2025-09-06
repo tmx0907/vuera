ca43e01909bcd626fc31fa1b1522e8114713d511
tmx0907 <tmx0907@gmail.com>
2025-09-07 00:17:11 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',              // 자산 경로 깨짐 방지
-  build: {
-    outDir: 'dist'        // Vercel은 dist를 Output Directory로 사용
-  }
-})
+  build: { outDir: "dist" }
+});
+
