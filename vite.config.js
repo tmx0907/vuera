31686a2ef3915161b84cd63e9c4ed9823cad2900
tmx0907 <tmx0907@gmail.com>
2025-09-07 00:17:56 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// ⚠️ root, build.rollupOptions.input 같은 커스텀은 일단 없애요.
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: "dist" }
+  publicDir: "public",   // 정적 파일 폴더
+  build: { outDir: "dist" }  // 빌드 출력 폴더
 });
 
